de9769f00037ed24fb93f179dd2ed6016e42068e
Ben Koala <bkoala123@hotmail.com>
2021-07-17 21:44:10 -0700

    Removed borders from border focus

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function compare_time(){
   let currentTime=moment().format("HH:mm").split(":");
   var totCurrentime=(Number(currentTime[0]) *60)+ Number(currentTime[1]);
   
-  totCurrentime=400;
+  totCurrentime=800;
  //Find out the current time of day for updates
   for(ii=9;ii<=17;ii++){
     //convert hours to mn
